be4e5b54829cce4cc508c58bba5fb4320ea3f68d
Generoman <53780885+Generoman@users.noreply.github.com>
2022-02-25 15:19:52 +0100

    fixed frontend bug

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(
-  <h1>Hello Darkness, My Old Friend</h1>,
-  document.getElementById("app")
-);
+function FrontPage() {
+  return (
+    <div>
+      <h1>MyApp</h1>
+      <div>
+        <Link to={"/login"}>Login</Link>
+      </div>
+      <div>
+        <Link to={"/register"}>Register</Link>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return <h1>Login</h1>;
+}
+
+function Register() {
+  return <h1>Register</h1>;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/register"} element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
