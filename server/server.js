c43c34d7cc6a9574b945f6f5d8bef2f2cdbad67a
Generoman <53780885+Generoman@users.noreply.github.com>
2022-02-25 14:54:45 +0100

    heroku setup ready

--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/login", (req, res) => {
+  res.json({ username: "admin", fullName: "Tøffetøffe Tøffetøff" });
+});
+
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on http://localhost:${server.address().port}`);
+  console.log(
+    `Non-client server running at http://localhost:${server.address().port}`
+  );
 });
