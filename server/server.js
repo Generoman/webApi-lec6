bc80a0e775f04fd2566784438cb36c7f6b26f1d3
Generoman <53780885+Generoman@users.noreply.github.com>
2022-02-25 15:30:39 +0100

    serving react from express

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 
 app.get("/api/login", (req, res) => {
   res.json({ username: "admin", fullName: "Tøffetøffe Tøffetøff" });
+});
+
+app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api/")) {
+    return res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
